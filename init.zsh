6079d060b55254f0d53da5f9b582635b03510d1b
Zou Alex <>
2019-12-11 21:20:42 +0800

    update custom functions and init file

--- init.zsh
+++ init.zsh
@@ -5,15 +5,34 @@
 # any custom stuff should go here.
 # ensure that 'custom' exists in the zmodules array in your .zimrc
 
+# Instead of export PATH=/some/path you probably want export PATH="$PATH:/some/path", 
+# unless you intend to clear out the system preset path completely.
+# export PATH is used to other programs to see the environment variables not only shell.
+PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
+
+ds=~/Desktop
+repo=~/Documents/gitRepo
+cw=~/Documents/CourseMaterials/2_semester
+zim=~/.zim/modules/custom
+node=s1702794@student.compute.inf.ed.ac.uk
 
 alias c='clear'
-alias sb='sublime'
+alias sb='sublime .'
+alias sbb='sublime'
 alias e='emacsclient -t'
 alias ec='emacsclient -c'
 alias emacsd='emacs --daemon'
-alias cdd='cd ~/Desktop/'
+alias cdd='cd ~/Desktop'
+alias op='open .'
+alias cpwd='pwd | pbcopy'
+alias dc='cd ${OLDPWD}'
 
 # Information Support
+alias kinit='kinit s1702794@INF.ED.AC.UK'
+# alias cirrus='s1702794@cirrus-msc.epcc.ed.ac.uk'
+# alias dice='s1702794@student.ssh.inf.ed.ac.uk'
+# alias student='s1702794@student.compute.inf.ed.ac.uk'
+# alias cluster='s1702794@mlp.inf.ed.ac.uk'
 
 
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=4'
